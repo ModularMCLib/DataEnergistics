c113e675dac174d216ab961c1b4fc82e56cfefe4
QiuYe <qiuye2024github@163.com>
2026-09-13 22:32:00 +0800

    spotless

--- src/main/java/com/fish_dan_/data_energistics/part/AdaptivePatternProviderPart.java
+++ src/main/java/com/fish_dan_/data_energistics/part/AdaptivePatternProviderPart.java
@@ -45,8 +45,8 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.fluids.capability.IFluidHandler;
 import net.neoforged.neoforge.items.IItemHandler;
 

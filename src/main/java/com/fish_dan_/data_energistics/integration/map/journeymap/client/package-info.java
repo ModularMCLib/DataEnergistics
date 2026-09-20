bdb4ec0163ef104b490d07a573b3286dc9ff8383
QiuYe <qiuye2024github@163.com>
2026-09-21 01:10:52 +0800

    清理集成迁移后仅剩包声明的空包

--- src/main/java/com/fish_dan_/data_energistics/integration/map/journeymap/client/package-info.java
+++
@@ -1,5 +0,0 @@
-/** JourneyMap v2 client plugin for orbital target selection and direct fire-control previews. */
-@NullMarked
-package com.fish_dan_.data_energistics.integration.map.journeymap.client;
-
-import org.jspecify.annotations.NullMarked;

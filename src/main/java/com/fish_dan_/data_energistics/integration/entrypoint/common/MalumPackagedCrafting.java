0f0c532e1f746e6b8a62b656a998581cc9e3a660
QiuYe <qiuye2024github@163.com>
2026-09-23 15:17:53 +0800

    移除 Malum 耐久物品复用注册

--- src/main/java/com/fish_dan_/data_energistics/integration/entrypoint/common/MalumPackagedCrafting.java
+++ src/main/java/com/fish_dan_/data_energistics/integration/entrypoint/common/MalumPackagedCrafting.java
@@ -6,14 +6,14 @@ import com.fish_dan_.data_energistics.api.entrypoint.DataEnergisticsRegistry;
 import com.fish_dan_.data_energistics.integration.magic.malum.packaged.MalumMachineAdapter;
 import com.fish_dan_.data_energistics.integration.magic.malum.packaged.MalumMachineKind;
 import com.fish_dan_.data_energistics.integration.magic.malum.packaged.RunicWorkbenchAdapter;
-import com.fish_dan_.data_energistics.integration.magic.malum.reusable.MalumReusableInputs;
 
 @DataEnergisticsEntrypoint(requiredMods = "malum")
 public final class MalumPackagedCrafting implements DataEnergisticsPlugin {
 
     @Override
     public void register(DataEnergisticsRegistry registry) {
-        registry.reusableInputs().register(new MalumReusableInputs());
+        // Spirit Focusing applies an instability-dependent random durability cost.
+        // Its tools cannot use fixed-damage custody.
         registry.packagedCrafting().register(new MalumMachineAdapter(MalumMachineKind.ALTAR));
         registry.packagedCrafting().register(new MalumMachineAdapter(MalumMachineKind.CRUCIBLE));
         registry.packagedCrafting().register(new RunicWorkbenchAdapter());
